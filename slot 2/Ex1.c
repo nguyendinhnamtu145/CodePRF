78c8ef1d2c874ff972a7083376802c23ead75345
unknown <nguyendinhnamtu16@gmail.com>
2026-01-09 11:03:08 +0700

    add all file in slot 2

---
+++ slot 2/Ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int a; //Khai bao bien so nguyen
+	a = 14; //Khoi tao gia tri bat ki
+	printf("Gia tri cua a la: %d", a); //in ra man hinh
+	return 0;
+}
---
+++ slot 2/Ex2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	double a; //Khai bao gia tri a la kieu so thuc
+	a = 145.2006; //Khoi tao so thuc a
+	printf (" Gia tri cua a la: %lf", a);
+	return 0;	
+}
