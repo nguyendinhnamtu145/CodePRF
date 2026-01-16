638d1a900b153215b1deff0d1e4b2f9846e28711
unknown <nguyendinhnamtu16@gmail.com>
2026-01-16 11:50:39 +0700

    add slot 4 folder

---
+++ slot 4/Ex4.c
@@ -0,0 +1,23 @@
+//Nhap 2 so nguyen a, b. In ra ket qua phep toan /. (Neu b=0 thi in "Khong the chia cho 0")
+
+#include <stdio.h>
+int main()
+{
+	int a, b;
+	char pt;
+	printf("Nhap a, b: ");
+	scanf (" %d, %d",&a ,&b);
+	float kq = (float)a/b;
+	printf ("Nhap phep toan:");
+	scanf ("Nhap phep toan: %c",pt);
+	
+	if (b!=0)
+	{
+		printf("Ket qua: %d %c %d = %.02f",a,pt,b,kq);
+	}
+	else
+	{
+		printf("Khong the chia cho 0");
+	}
+	return 0;
+}
